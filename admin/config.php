5c47207f31274ca60403d44f88e572296f1a7d27
Vitaliy Godzevich <v.godzevich@interkassa.com>
2018-07-09 17:47:04 +0300

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc.store:8000/admin/');
+define('HTTP_CATALOG', 'http://oc.store:8000/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc.store:8000/admin/');
+define('HTTPS_CATALOG', 'http://oc.store:8000/');
+
+// DIR
+define('DIR_APPLICATION', '/home/phpdev021/oc.store/admin/');
+define('DIR_SYSTEM', '/home/phpdev021/oc.store/system/');
+define('DIR_IMAGE', '/home/phpdev021/oc.store/image/');
+define('DIR_LANGUAGE', '/home/phpdev021/oc.store/admin/language/');
+define('DIR_TEMPLATE', '/home/phpdev021/oc.store/admin/view/template/');
+define('DIR_CONFIG', '/home/phpdev021/oc.store/system/config/');
+define('DIR_CACHE', '/home/phpdev021/oc.store/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/phpdev021/oc.store/system/storage/download/');
+define('DIR_LOGS', '/home/phpdev021/oc.store/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/phpdev021/oc.store/system/storage/modification/');
+define('DIR_UPLOAD', '/home/phpdev021/oc.store/system/storage/upload/');
+define('DIR_CATALOG', '/home/phpdev021/oc.store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ik2');
+define('DB_PASSWORD', 'ik2');
+define('DB_DATABASE', 'oc.store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc.store:8000/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc.store:8000/');
+
+// DIR
+define('DIR_APPLICATION', '/home/phpdev021/oc.store/catalog/');
+define('DIR_SYSTEM', '/home/phpdev021/oc.store/system/');
+define('DIR_IMAGE', '/home/phpdev021/oc.store/image/');
+define('DIR_LANGUAGE', '/home/phpdev021/oc.store/catalog/language/');
+define('DIR_TEMPLATE', '/home/phpdev021/oc.store/catalog/view/theme/');
+define('DIR_CONFIG', '/home/phpdev021/oc.store/system/config/');
+define('DIR_CACHE', '/home/phpdev021/oc.store/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/phpdev021/oc.store/system/storage/download/');
+define('DIR_LOGS', '/home/phpdev021/oc.store/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/phpdev021/oc.store/system/storage/modification/');
+define('DIR_UPLOAD', '/home/phpdev021/oc.store/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ik2');
+define('DB_PASSWORD', 'ik2');
+define('DB_DATABASE', 'oc.store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
